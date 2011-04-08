d375755fc4c463efd67f6773024ed6e2941795c8
stealth977 <stealth977@localhost>
2011-04-08 12:30:40 +0300

    - Added arrowdodger's linux Cmake fix - Hydrax fix : changed values were not being saved properly, now they are saved into ofs (added functions to original hydrax code to retrieve the config as a string)

--- Dependencies/HYDRAX/Hydrax/src/Hydrax/CfgFileManager.h
+++ Dependencies/HYDRAX/Hydrax/src/Hydrax/CfgFileManager.h
@@ -61,6 +61,12 @@ namespace Hydrax
 		 */
 		const bool save(const Ogre::String& File, const Ogre::String& Path = "") const;
 
+		/** Return current hydrax config as string 
+		    @param Data Destination string to save config in
+			@return false if an error has been ocurred(Check the log file in this case).
+		 */
+        const bool getSaveString(Ogre::String& Data) const;
+
 		/** <int> Get the cfg string
 		    @param Name Parameter name
 		    @param Value Parameter value
