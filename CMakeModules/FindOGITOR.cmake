8f1b0b500ae4c8693d6b5b9aa806c60b05c11765
jacmoe <jacmoe@localhost>
2012-04-06 19:44:47 +0200

    Initial FindOGITOR cmake script

---
+++ CMakeModules/FindOGITOR.cmake
@@ -0,0 +1,44 @@
+######################################################################
+# OGITOR BUILD SYSTEM
+# Welcome to the CMake build system for OGITOR.
+# This is the main file where we prepare the general build environment
+# and provide build configuration options.
+######################################################################
+
+# - Try to find Ogitor
+# Once done, this will define
+#
+#  OGITOR_FOUND - system has Ogitor
+#  OGITOR_INCLUDE_DIRS - the Ogitor include directories
+#  OGITOR_LIBRARIES - link these to use Ogitor
+
+include(FindPkgMacros)
+findpkg_begin(OGITOR)
+
+# Get path, convert backslashes as ${ENV_${var}}
+getenv_path(OGITOR_HOME)
+
+# construct search paths
+set(OGITOR_PREFIX_PATH ${OGITOR_HOME} ${ENV_OGITOR_HOME} /usr/local /usr/local/include /usr/local/lib /usr/include /usr/lib /usr/local/include/ogitor /usr/include/ogitor /usr/lib/ogitor /usr/local/lib/ogitor)
+create_search_paths(OGITOR)
+# redo search if prefix path changed
+clear_if_changed(OGITOR_PREFIX_PATH
+  OGITOR_LIBRARY_REL
+  OGITOR_LIBRARY_DBG
+  OGITOR_INCLUDE_DIR
+)
+
+set(OGITOR_LIBRARY_NAMES ogitor)
+get_debug_names(OGITOR_LIBRARY_NAMES)
+
+use_pkgconfig(OGITOR_PKGC OGITOR)
+
+findpkg_framework(OGITOR)
+
+find_path(OGITOR_INCLUDE_DIR NAMES ogitor.h HINTS ${OGITOR_INC_SEARCH_PATH} ${OGITOR_PKGC_INCLUDE_DIRS} PATH_SUFFIXES ogitor)
+find_library(OGITOR_LIBRARY_REL NAMES ${OGITOR_LIBRARY_NAMES} HINTS ${OGITOR_LIB_SEARCH_PATH} ${OGITOR_PKGC_LIBRARY_DIRS} PATH_SUFFIXES "" release relwithdebinfo minsizerel)
+find_library(OGITOR_LIBRARY_DBG NAMES ${OGITOR_LIBRARY_NAMES_DBG} HINTS ${OGITOR_LIB_SEARCH_PATH} ${OGITOR_PKGC_LIBRARY_DIRS} PATH_SUFFIXES "" debug)
+make_library_set(OGITOR_LIBRARY)
+
+findpkg_finish(OGITOR)
+add_parent_dir(OGITOR_INCLUDE_DIRS OGITOR_INCLUDE_DIR)
