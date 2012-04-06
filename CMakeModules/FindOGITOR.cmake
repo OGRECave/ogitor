827d1043421b3583b2cae55e44ddd05b6986d084
jacmoe <jacmoe@localhost>
2012-04-06 20:13:30 +0200

    FindOGITOR script - is able to find Ogitor include dirs and libs, with a bit of help

--- CMakeModules/FindOGITOR.cmake
+++ CMakeModules/FindOGITOR.cmake
@@ -19,7 +19,7 @@ findpkg_begin(OGITOR)
 getenv_path(OGITOR_HOME)
 
 # construct search paths
-set(OGITOR_PREFIX_PATH ${OGITOR_HOME} ${ENV_OGITOR_HOME} /usr/local /usr/local/include /usr/local/lib /usr/include /usr/lib /usr/local/include/ogitor /usr/include/ogitor /usr/lib/ogitor /usr/local/lib/ogitor)
+set(OGITOR_PREFIX_PATH ${OGITOR_HOME}/Ogitor ${ENV_OGITOR_HOME} /home/jacmoe/programming/ogitor/Ogitor /usr/local /usr/local/include /usr/local/lib /usr/include /usr/lib /usr/local/include/ogitor /usr/include/ogitor /usr/lib/ogitor /usr/local/lib/ogitor /home/jacmoe/programming/ogitor/build)
 create_search_paths(OGITOR)
 # redo search if prefix path changed
 clear_if_changed(OGITOR_PREFIX_PATH
@@ -28,14 +28,14 @@ clear_if_changed(OGITOR_PREFIX_PATH
   OGITOR_INCLUDE_DIR
 )
 
-set(OGITOR_LIBRARY_NAMES ogitor)
+set(OGITOR_LIBRARY_NAMES Ogitor)
 get_debug_names(OGITOR_LIBRARY_NAMES)
 
 use_pkgconfig(OGITOR_PKGC OGITOR)
 
 findpkg_framework(OGITOR)
 
-find_path(OGITOR_INCLUDE_DIR NAMES ogitor.h HINTS ${OGITOR_INC_SEARCH_PATH} ${OGITOR_PKGC_INCLUDE_DIRS} PATH_SUFFIXES ogitor)
+find_path(OGITOR_INCLUDE_DIR NAMES Ogitors.h HINTS ${OGITOR_INC_SEARCH_PATH} ${OGITOR_PKGC_INCLUDE_DIRS} PATH_SUFFIXES Ogitor)
 find_library(OGITOR_LIBRARY_REL NAMES ${OGITOR_LIBRARY_NAMES} HINTS ${OGITOR_LIB_SEARCH_PATH} ${OGITOR_PKGC_LIBRARY_DIRS} PATH_SUFFIXES "" release relwithdebinfo minsizerel)
 find_library(OGITOR_LIBRARY_DBG NAMES ${OGITOR_LIBRARY_NAMES_DBG} HINTS ${OGITOR_LIB_SEARCH_PATH} ${OGITOR_PKGC_LIBRARY_DIRS} PATH_SUFFIXES "" debug)
 make_library_set(OGITOR_LIBRARY)
