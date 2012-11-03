d205f48cb07e262e74f07099995063a591c8e596
Andrew Fenn <andrewfenn@gmail.com>
2012-11-03 14:37:57 +0700

    Added find Ogitor and OFS cmake scripts for plugins

--- CMakeModules/install/unix.cmake
+++ CMakeModules/install/unix.cmake
@@ -1,51 +1,45 @@
 # SDK : Ogitor includes
-FILE(GLOB ogitor_headers
-    "${CMAKE_CURRENT_SOURCE_DIR}/Ogitor/include/*.h")
-        install(FILES ${ogitor_headers}
+file(GLOB ogitor_headers "${CMAKE_CURRENT_SOURCE_DIR}/Ogitor/include/*.h")
+install(FILES ${ogitor_headers}
     DESTINATION
         ${CMAKE_INSTALL_PREFIX}/include/ogitor
     CONFIGURATIONS Debug Release RelWithDebInfo MinSizeRel
 )
 
 # SDK: OFS headers
-FILE(GLOB ofs_headers
-    "${CMAKE_CURRENT_SOURCE_DIR}/Dependencies/OFS/include/*.h")
-        install(FILES ${ofs_headers}
+file(GLOB ofs_headers "${CMAKE_CURRENT_SOURCE_DIR}/Dependencies/OFS/include/*.h")
+install(FILES ${ofs_headers}
     DESTINATION
-        ${CMAKE_INSTALL_PREFIX}/include/ogitor/dependencies/OFS
+        ${CMAKE_INSTALL_PREFIX}/include/OFS
     CONFIGURATIONS Debug Release RelWithDebInfo MinSizeRel
 )
 
 # SDK: ImageConverter headers
-FILE(GLOB ImageConverter_headers
-    "${CMAKE_CURRENT_SOURCE_DIR}/Dependencies/ImageConverter/include/*.hxx")
-        install(FILES ${ImageConverter_headers}
+file(GLOB ImageConverter_headers "${CMAKE_CURRENT_SOURCE_DIR}/Dependencies/ImageConverter/include/*.hxx")
+install(FILES ${ImageConverter_headers}
     DESTINATION
         ${CMAKE_INSTALL_PREFIX}/include/ogitor/dependencies/ImageConverter
     CONFIGURATIONS Debug Release RelWithDebInfo MinSizeRel
 )
 
 # SDK: GenericImageEditor headers
-FILE(GLOB GenericImageEditor_headers
-    "${CMAKE_CURRENT_SOURCE_DIR}/Dependencies/GenericImageEditor/include/*.hxx")
-        install(FILES ${GenericImageEditor_headers}
+file(GLOB GenericImageEditor_headers "${CMAKE_CURRENT_SOURCE_DIR}/Dependencies/GenericImageEditor/include/*.hxx")
+install(FILES ${GenericImageEditor_headers}
     DESTINATION
         ${CMAKE_INSTALL_PREFIX}/include/ogitor/dependencies/GenericImageEditor
     CONFIGURATIONS Debug Release RelWithDebInfo MinSizeRel
 )
 
 # SDK: GenericTextEditor headers
-FILE(GLOB GenericTextEditor_headers
-    "${CMAKE_CURRENT_SOURCE_DIR}/Dependencies/GenericTextEditor/include/*.hxx")
-        install(FILES ${GenericTextEditor_headers}
+file(GLOB GenericTextEditor_headers "${CMAKE_CURRENT_SOURCE_DIR}/Dependencies/GenericTextEditor/include/*.hxx")
+install(FILES ${GenericTextEditor_headers}
     DESTINATION
         ${CMAKE_INSTALL_PREFIX}/include/ogitor/dependencies/GenericTextEditor
     CONFIGURATIONS Debug Release RelWithDebInfo MinSizeRel
 )
 
 # SDK: Hydrax headers
-install(DIRECTORY
-    ${CMAKE_CURRENT_SOURCE_DIR}/Dependencies/HYDRAX/Hydrax/src/Hydrax
+install(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/Dependencies/HYDRAX/Hydrax/src/Hydrax
     DESTINATION ${CMAKE_INSTALL_PREFIX}/include/ogitor/dependencies
     CONFIGURATIONS Debug Release RelWithDebInfo MinSizeRel
     PATTERN "*.cpp" EXCLUDE
@@ -61,25 +55,22 @@ install(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/Dependencies/SkyX
 )
 
 # SDK: PagedGeometry headers
-FILE(GLOB pg_headers
-    "${CMAKE_CURRENT_SOURCE_DIR}/Dependencies/PagedGeometry/include/*.h")
-        install(FILES ${pg_headers}
+file(GLOB pg_headers "${CMAKE_CURRENT_SOURCE_DIR}/Dependencies/PagedGeometry/include/*.h")
+install(FILES ${pg_headers}
     DESTINATION
         ${CMAKE_INSTALL_PREFIX}/include/ogitor/dependencies/PagedGeometry
     CONFIGURATIONS Debug Release RelWithDebInfo MinSizeRel
 )
 
 # SDK: Angelscript headers
-FILE(GLOB angelscript_headers
-    "${CMAKE_CURRENT_SOURCE_DIR}/Dependencies/Angelscript/include/*.h")
-        install(FILES ${angelscript_headers}
+file(GLOB angelscript_headers "${CMAKE_CURRENT_SOURCE_DIR}/Dependencies/Angelscript/include/*.h")
+install(FILES ${angelscript_headers}
     DESTINATION
         ${CMAKE_INSTALL_PREFIX}/include/ogitor/dependencies/Angelscript
     CONFIGURATIONS Debug Release RelWithDebInfo MinSizeRel
 )
 
-install(DIRECTORY
-    ${CMAKE_CURRENT_SOURCE_DIR}/Dependencies/Angelscript/add_on
+install(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/Dependencies/Angelscript/add_on
     DESTINATION
     ${CMAKE_INSTALL_PREFIX}/include/ogitor/dependencies/Angelscript
     CONFIGURATIONS Debug Release RelWithDebInfo MinSizeRel
@@ -92,9 +83,8 @@ install(DIRECTORY
 )
 
 # SDK: Caelum headers
-FILE(GLOB caelum_headers
-    "${CMAKE_CURRENT_SOURCE_DIR}/Dependencies/Caelum-0.5.0/main/include/*.h")
-        install(FILES ${caelum_headers}
+file(GLOB caelum_headers "${CMAKE_CURRENT_SOURCE_DIR}/Dependencies/Caelum-0.5.0/main/include/*.h")
+install(FILES ${caelum_headers}
     DESTINATION
         ${CMAKE_INSTALL_PREFIX}/include/ogitor/dependencies/Caelum-0.5.0
     CONFIGURATIONS Debug Release RelWithDebInfo MinSizeRel
