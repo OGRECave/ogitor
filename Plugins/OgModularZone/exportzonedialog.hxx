cef0beeffab413f7c8e363bf6ed10e0e6f8accf4
Philip Allgaier <mail@spacegaier.de>
2014-09-11 21:46:45 +0200

    fixes Unix CMake compilation issue + reduces nesting of Hydrax folders; fixes #1023

---
+++ Plugins/OgModularZone/exportzonedialog.hxx
@@ -0,0 +1,29 @@
+#ifndef EXPORTZONEDIALOG_H
+#define EXPORTZONEDIALOG_H
+
+#include <QtWidgets/QDialog>
+#include "ui_exportzonedialog.h"
+#include "OgreString.h"
+
+//namespace Ui {
+//    class ExportZoneDialog;
+//}
+
+class ExportZoneDialog : public QDialog,public Ui::ExportZoneDialog{
+    Q_OBJECT
+public:
+    ExportZoneDialog(QWidget *parent = 0);
+    ~ExportZoneDialog();
+    Ogre::String getShortDescription(void){return m_ui->lineEditShortDesc->text().toStdString();}
+    Ogre::String getLongDescription(void){return m_ui->plainTextEditLongDesc->toPlainText().toStdString();}
+    Ogre::String getFileName(void){return m_ui->lineEditFileName->text().toStdString();}
+
+
+protected:
+    void changeEvent(QEvent *e);
+
+private:
+    Ui::ExportZoneDialog *m_ui;
+};
+
+#endif // EXPORTZONEDIALOG_H
