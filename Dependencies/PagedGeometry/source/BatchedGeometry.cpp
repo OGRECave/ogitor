14b332307c59a1921d93558bc4abeb9a18864c1c
Transporter <Transporter@localhost>
2014-09-30 17:14:13 +0200

    Fixes for Ogre 1.10 + CMake bugfixes

--- Dependencies/PagedGeometry/source/BatchedGeometry.cpp
+++ Dependencies/PagedGeometry/source/BatchedGeometry.cpp
@@ -472,7 +472,7 @@ void BatchedGeometry::SubBatch::addSubEntity(SubEntity *ent, const Vector3 &posi
 				case VET_COLOUR_ABGR:
 					break;
 				default:
-					OGRE_EXCEPT(0, "Unknown RenderSystem color format", "BatchedGeometry::SubBatch::addSubMesh()");
+					OGRE_EXCEPT(Ogre::Exception::ERR_RENDERINGAPI_ERROR, "Unknown RenderSystem color format", "BatchedGeometry::SubBatch::addSubMesh()");
 					break;
 		}
 	}
