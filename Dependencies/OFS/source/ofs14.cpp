e9aafeabe26e4c47ad8f14a16d115ceeb9498727
stealth977 <stealth977@localhost>
2015-02-12 11:31:56 +0200

    - TerrainLoadManager workaround

--- Dependencies/OFS/source/ofs14.cpp
+++ Dependencies/OFS/source/ofs14.cpp
@@ -1746,7 +1746,7 @@ namespace OFS
 
             if(open_mode & OFS_WRITE)
             {
-                if((fileDesc->UseCount > 0) || (fileDesc->Flags & OFS_READONLY) || (fileDesc->Flags & OFS_LINK))
+                if(((fileDesc->UseCount > 0) && !(open_mode & OFS_FORCE)) || (fileDesc->Flags & OFS_READONLY) || (fileDesc->Flags & OFS_LINK))
                     return OFS_ACCESS_DENIED;
 
                 fileDesc->WriteLocked = true;
